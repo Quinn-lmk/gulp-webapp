27217af96eb8f87246203f511fc8cfddebfda0cf
Quinn <lmk445635121@hotmail.com>
2016-09-06 11:43:48 +0800

    add 单元测试

---
+++ test/spec/test.js
@@ -0,0 +1,11 @@
+(function () {
+  'use strict';
+
+  describe('Give it some context', function () {
+    describe('maybe a bit more context here', function () {
+      it('should run here few assertions', function () {
+        expect(add(1,0)).to.be.equal(1);
+      });
+    });
+  });
+})();
